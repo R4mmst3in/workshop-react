c58bbcc337114c98411f7b4f2b819bf030f32fbb
R4mmst3in <pjmarpe@gmail.com>
2022-04-06 14:41:34 +0200

    Dia 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { useState, useEffect } from "react";
+import { Heading, Image } from "@chakra-ui/react";
 import * as API from "./services/launches.js";
+import { LaunchItem } from "./components/LaunchItem.jsx";
+
 import logo from "./assets/index.png";
 
 export function App() {
   const [launches, setLaunches] = useState([]);
 
   useEffect(() => {
-    API.getAllLaunches().then(setLaunches).catch(console.log);
+    API.getAllLaunches().then(setLaunches);
   }, []);
-
+  
   return (
     <>
-      <img src={logo} width={300} />
-      <h1>SpaceX Launches</h1>
-      <ul>
+      <Image src={logo} m={4} width={250} />
+      <Heading align="center" as='h1' size='lg' m={4}>
+        SpaceX Launches
+      </Heading>
+      <section>
         {launches.map((launch) => (
-          <li key={launch.flight_number}>
-            {launch.mission_name} ({launch.launch_year})
-          </li>
+          <LaunchItem key={launch.flight_number} {...launch} />
         ))}
-      </ul>
+      </section>
     </>
   );
 }
